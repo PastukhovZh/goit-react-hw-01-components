533e2b38c6eb38998c3d358d9c25eeb9acafaf10
Zhenya Pastukhov <amamahahatala@hotmail.com>
2022-09-25 10:18:39 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
-import { AppWrap,AppItems, AppColumn } from "./App.styled";
-import { Profile } from "./Profile/Profile"
-import user from './Profile/user.json'
-import { Statistics } from "./Statistics/Statistics";
-import data from './Statistics/data.json'
-import { FriendList } from "./FriendList/FriendList";
-import friends from './FriendList/friends.json'
-import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
-import transactions from './TransactionHistory/transactions.json';
+import { AppWrap,AppItems, AppColumn } from "components/App.styled";
+import { Profile } from "components/Profile/Profile"
+import user from 'components/Profile/user.json'
+import { Statistics } from "components/Statistics/Statistics";
+import data from 'components/Statistics/data.json'
+import { FriendList } from "components/FriendList/FriendList";
+import friends from 'components/FriendList/friends.json'
+import { TransactionHistory } from "components/TransactionHistory/TransactionHistory";
+import transactions from 'components/TransactionHistory/transactions.json';
 
 export const App = () => {
   return (
